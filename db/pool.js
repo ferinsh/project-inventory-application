a243a0856ace681b6ae4aac623b8f730e2b6c7cc
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2024-09-13 00:12:47 +0530

    test

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,27 @@
 const {Pool} = require("pg");
 require('dotenv').config()
 
+// module.exports = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+//     ssl: {
+//         rejectUnauthorized: false // Change to true if you have a valid SSL certificate
+//     },
+//     max: 20, // Max number of connections in the pool
+//     idleTimeoutMillis: 30000, // Idle timeout in milliseconds
+//     connectionTimeoutMillis: 2000 // Connection timeout in milliseconds
+// });
+
+// module.exports = new Pool({
+//     // connectionString: "postgresql://<role_name>:<role_password>@localhost:5432/top_users"
+//     connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@dpg-${process.env.DB_HOST}/${process.env.DB_DATABSE}`
+//   });
+
 module.exports = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     ssl: {
         rejectUnauthorized: false // Change to true if you have a valid SSL certificate
     },
