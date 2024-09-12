6585522e41afb779ce20451a1a4ca23769b75624
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2024-09-12 23:15:35 +0530

    test

--- db/queries.js
+++ db/queries.js
@@ -1,23 +1,29 @@
-// db/queries.js
 const pool = require('./pool');
 
+// Function to insert a new track
 async function addTrack({ title, artist, duration, image }) {
-    const query = `INSERT INTO tracks (title, artist, duration, image) VALUES ($1, $2, $3, $4);`;
+    const query = `
+        INSERT INTO tracks (title, artist, duration, image)
+        VALUES ($1, $2, $3, $4);
+    `;
     try {
         await pool.query(query, [title, artist, duration, image]);
     } catch (err) {
         console.error('Error executing query:', err);
-        throw err;
+        throw err; // Rethrow to handle it in the controller
     }
 }
 
+// Function to get all rows from table tracks 
 async function getAllTracks() {
-    const query = `SELECT * FROM tracks;`;
+    const query = `
+        SELECT * FROM tracks;
+    `;
     try {
         const result = await pool.query(query);
         return result.rows;
     } catch (err) {
-        console.error('Error executing query: ', err);
+        console.error('Error executing query:', err);
         throw err;
     }
 }
@@ -28,7 +34,7 @@ async function searchTrackByTitle(title) {
         const result = await pool.query(query, [`%${title}%`]);
         return result.rows;
     } catch (err) {
-        console.error(err);
+        console.error('Error executing query:', err);
         throw err;
     }
 }
--- routes/searchTrackRouter.js
+++ routes/searchTrackRouter.js
@@ -1,22 +1,16 @@
-const {Router} = require("express");
-
-const searchTitleController = require("../controllers/searchTitleController")
+const { Router } = require("express");
+const searchTitleController = require("../controllers/searchTitleController");
 
 const searchTrackRouter = Router();
 
 searchTrackRouter.get("/", (req, res) => {
     res.render("index.ejs", {
         show: "specificTrackForm"
-    })
-})
+    });
+});
 
 searchTrackRouter.post("/", (req, res) => {
-    // const title = req.body.title;
-    // console.log(title);
-    
-    // res.redirect("/");
     searchTitleController.searchByTitle(req, res);
-    
-})
+});
 
-module.exports = searchTrackRouter
+module.exports = searchTrackRouter;
